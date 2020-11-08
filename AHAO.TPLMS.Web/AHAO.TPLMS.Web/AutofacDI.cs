494f86c2d827d9c6896f4f95afe699257ab207cf
ahao214 <haochen214@163.com>
2020-11-08 21:52:52 +0800

    用Autofac替换内置的DI框架

---
+++ AHAO.TPLMS.Web/AHAO.TPLMS.Web/AutofacDI.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using AHAO.TPLMS.Service;
+using AHAO.TPLMS.DataBase;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace AHAO.TPLMS.Web
+{
+    public class AutofacDI:Autofac.Module
+    {
+        //重写Autofac管道的Load方法，在这里注册注入
+        protected override void Load(ContainerBuilder builder)
+        {
+            //注册服务的对象，这里以命名空间名称中含有AHAO.TPLMS.Service和Repository字符串为标志，否则注册失败
+            builder.RegisterAssemblyTypes(Assembly.GetAssembly(typeof(AuthoriseService)))
+                .Where(u => u.Namespace == "AHAO.TPLMS.Service");
+
+            builder.RegisterAssemblyTypes(GetAssemblyByName("AHAO.TPLMS.Repository")).
+                Where(a => a.Namespace.EndsWith("Repository")).AsImplementedInterfaces();
+            //base.Load(builder);
+        }
+
+
+        /// <summary>
+        /// 根据程序集名称获取程序集
+        /// </summary>
+        /// <param name="AssemblyName"></param>
+        /// <returns></returns>
+        public static Assembly GetAssemblyByName(String AssemblyName)
+        {
+            return Assembly.Load(AssemblyName);
+        }
+
+    }
+}
--- AHAO.TPLMS.Web/AHAO.TPLMS.Web/Startup.cs
+++ AHAO.TPLMS.Web/AHAO.TPLMS.Web/Startup.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AHAO.TPLMS.DataBase;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -25,12 +28,19 @@ namespace AHAO.TPLMS.Web
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            //services.AddControllers();
             //services.AddDbContext<TPLMSDbContext>(Options => Options.UseSqlServer(Configuration.GetConnectionString("TPLMSDbContext")));
             services.AddDbContext<TPLMSDbContext>(options=>options.UseSqlServer("server=.\\sqlexpress;database=TPLMS;uid=sa;pwd=hao@chen214", b => b.MigrationsAssembly("AHAO.TPLMS.Web")));
-            services.AddMvc();                                 
+            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            return RegisterAutofac(services);   //注册Autofac
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,15 +50,61 @@ namespace AHAO.TPLMS.Web
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseRouting();           
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            else
             {
-                endpoints.MapControllers();
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=login}/{id?}");
             });
+
+            //app.UseRouting();           
+
+            //app.UseAuthorization();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
         }
+
+
+        private IServiceProvider RegisterAutofac(IServiceCollection services)
+        {
+            //实例化Autofac容器
+            var builder = new ContainerBuilder();
+            //将Services中的服务填充到Autofac中
+            builder.Populate(services);
+            //新模块组件注册
+            builder.RegisterModule<AutofacDI>();
+            //首先注册options,供DbContext服务初始化使用
+            builder.Register(c =>
+            {
+                var optionBuilder = new DbContextOptionsBuilder<TPLMSDbContext>();
+                optionBuilder.UseSqlServer(Configuration.GetConnectionString("TPLMSDbContext"));
+                return optionBuilder.Options;
+            }).InstancePerLifetimeScope();
+
+            //注册DbContext
+            builder.RegisterType<TPLMSDbContext>()
+                .AsSelf()
+                .InstancePerLifetimeScope();
+
+            //创建容器
+            var Container = builder.Build();
+
+            //第三方IoC接管Core内置DI容器
+            return new AutofacServiceProvider(Container);
+
+
+
+
+        }
+
     }
 }
