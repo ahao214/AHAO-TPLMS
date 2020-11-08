7241a0c9fc7526593de23e5737b578b1fcb9218a
ahao214 <haochen214@163.com>
2020-11-08 21:03:48 +0800

    给数据库添加初始数据

--- AHAO.TPLMS.Web/AHAO.TPLMS.Web/Program.cs
+++ AHAO.TPLMS.Web/AHAO.TPLMS.Web/Program.cs
@@ -1,9 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using AHAO.TPLMS.DataBase;
+using AHAO.TPLMS.Web.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +18,25 @@ namespace AHAO.TPLMS.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope= host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<TPLMSDbContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "数据库数据初始化错误！");                    
+                }
+            }
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
