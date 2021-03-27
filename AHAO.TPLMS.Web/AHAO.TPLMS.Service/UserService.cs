193006ccfa74071e86c097d65b93250035ed829b
ahao214 <haochen214@163.com>
2021-03-27 22:33:18 +0800

    用户管理

---
+++ AHAO.TPLMS.Web/AHAO.TPLMS.Service/UserService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AHAO.TPLMS.Contract;
+using AHAO.TPLMS.Entitys;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace AHAO.TPLMS.Service
+{
+    public class UserService
+    {
+        private IUserRepository _UserMgr;
+        private User _user;
+        private List<User> _users;  //用户列表
+
+        public UserService(IUserRepository userMgr)
+        {
+            _UserMgr = userMgr;
+        }
+
+        public dynamic LoadUser(int pageIndex,int pageSize)
+        {
+            //查询用户表
+            Expression<Func<User, bool>> exp = u => u.Id > 0;
+            var users = _UserMgr.Find(pageIndex, pageSize, exp);
+            int total = _UserMgr.GetCount(exp);
+            List<User> list = new List<User>();
+
+            foreach(var item in users  )
+            {
+                list.Add(item);
+            }
+
+            return new
+            {
+                total = total,
+                rows = list
+            };
+        }
+
+        #region 更新用户
+
+        public string Save(User u)
+        {
+            try
+            {                
+                _UserMgr.Update(u);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return "OK";
+        }
+
+        #endregion
+
+        #region 添加用户
+
+        public string Add(User user )
+        {
+            try
+            {                
+                _UserMgr.Add(user);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return "OK";
+        }
+
+        #endregion
+
+        #region 删除用户
+
+        public string Delete(User user)
+        {
+            try
+            {
+                _UserMgr.Delete(user);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return "OK";
+        }
+
+        #endregion 
+
+    }
+}
---
+++ AHAO.TPLMS.Web/AHAO.TPLMS.Web/Controllers/UserMgrController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AHAO.TPLMS.Service;
+using AHAO.TPLMS.Util.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AHAO.TPLMS.Entitys;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace AHAO.TPLMS.Web.Controllers
+{
+    public class UserMgrController : BaseController
+    {
+        UserService users;
+
+        public UserMgrController(UserService user)
+        {
+            users = user;            
+        }
+
+        // GET: UserMgr
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public String List()
+        {
+            var page = Request.Query["page"].FirstOrDefault();
+            var size = Request.Query["rows"].FirstOrDefault();
+            int pageIndxe = page == null ? 1 : int.Parse(page);
+            int pageSize = size == null ? 20 : int.Parse(size);
+            var userList = users.LoadUser(pageIndxe, pageSize);
+            var json = JsonHelper.Instance.Serialize(userList);
+            return json;
+        }
+
+        public ActionResult Update(User user )
+        {
+            string result = "NO";
+            try
+            {
+                //TODO:Add update logic here
+                result = users.Save(user);
+            }
+            catch
+            {
+
+            }
+            return Content(result);
+        }
+
+        public ActionResult Add(User user)
+        {
+            string result = "NO";
+            try
+            {
+                //TODO:Add logic here
+                result = users.Add(user);
+            }
+            catch
+            {
+
+            }
+            return Content(result);
+        }
+
+        public ActionResult Delete(User user )
+        {
+            string result = "NO";
+            try
+            {
+                //TODO:Add delete logic here 
+                result = users.Delete(user);
+            }
+            catch
+            { }
+            return Content(result);
+        }
+
+    }
+}
