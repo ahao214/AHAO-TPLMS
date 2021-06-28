07e2c4c0e296cc5bd8340e21a7ee8ad72f06025a
ahao214 <haochen214@163.com>
2021-06-28 21:25:35 +0800

    Module类

--- AHAO.TPLMS.Web/AHAO.TPLMS.Entitys/Module.cs
+++ AHAO.TPLMS.Web/AHAO.TPLMS.Entitys/Module.cs
@@ -1,24 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace AHAO.TPLMS.Entitys
 {
     public class Module
     {
-        public List<Module> GetModules(int pageindex, int pagesize = 1000)
+        public Module()
         {
-            //模块列表
-
-            var users = _moduleMgr.LoadModules(pageindex, pagesize);
-
-            List<Module> list = new List<Module>();
-            foreach (var item in users)
-            {
-                list.Add(item);
-            }
-
-            return list;
+            this.CascadeId = string.Empty;
+            this.Name = string.Empty;
+            this.Url = string.Empty;
+            this.HotKey = string.Empty;
+            this.ParentId = 0;
+            this .IconName = string.Empty;
+            this.Status = 0;
+            this .ParentName = string.Empty;
+            this .Vector = string.Empty;
+            this.SortNo = 0;            
         }
+
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string CascadeId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [StringLength(255)]
+        public string Url { get; set; }
+        [StringLength(255)]
+        public string HotKey { get; set; }
+        public int ParentId { get; set; }
+        public bool IsLeaf { get; set; }
+        public bool IsAutoExpand { get; set; }
+        [StringLength(255)]
+        public string IconName { get; set; }
+        public int Status { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string ParentName { get; set; }
+        [StringLength(255)]
+        public string Vector { get; set; }
+        public int SortNo { get; set; }
+
+
     }
 }
---
+++ AHAO.TPLMS.Web/AHAO.TPLMS.Web/Controllers/ModuleMgrController.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JST.TPLMS.Service;
+using JST.TPLMS.Util.Helpers;
+using JST.TPLMS.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JST.TPLMS.Web.Controllers
+{
+    public class ModuleMgrController : Controller
+    {
+        ModuleService moduleSer;
+        public ModuleMgrController(ModuleService modu)
+        { moduleSer = modu; }
+        // GET: ModuleMgr
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ModuleMgr/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+        public string List()
+        {
+            var page = Request.Form["page"].ToString();
+            var size = Request.Form["rows"].ToString();
+            int pageIndex = page == null ? 1 : int.Parse(page);
+            int pageSize = size == null ? 20 : int.Parse(size);
+            var userList = moduleSer.LoadModules(pageIndex, pageSize);
+            var json = JsonHelper.Instance.Serialize(userList);
+            return json;
+        }
+        public string GetPermit(string uid)
+        {
+            int userId = NumberHelper.ToInt(uid);
+            var page = Request.Form["page"].ToString();
+            var size = Request.Form["rows"].ToString();
+            int pageIndex = page == null ? 1 : int.Parse(page);
+            int pageSize = size == null ? 20 : int.Parse(size);
+            var moduleList = moduleSer.GetModules(pageIndex, pageSize);
+            var userList = moduleSer.LoadForUser(userId);
+            foreach (var item in userList)
+            {
+                var m = moduleList.Where(u => u.Id == item.Id).FirstOrDefault();
+                m.Checked = true;
+            }
+            var json = JsonHelper.Instance.Serialize(moduleList);
+            return json;
+        }
+        public string GetRolePermit(string rid)
+        {
+            int roleId = NumberHelper.ToInt(rid);
+            var page = Request.Form["page"].ToString();
+            var size = Request.Form["rows"].ToString();
+            int pageIndex = page == null ? 1 : int.Parse(page);
+            int pageSize = size == null ? 20 : int.Parse(size);
+            var moduleList = moduleSer.GetModules(pageIndex, pageSize);
+            var roleList = moduleSer.LoadForRole(roleId);
+            foreach (var item in roleList)
+            {
+                var m = moduleList.Where(u => u.Id == item.Id).FirstOrDefault();
+                m.Checked = true;
+            }
+            var json = JsonHelper.Instance.Serialize(moduleList);
+            return json;
+        }
+        // GET: ModuleMgr/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ModuleMgr/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ModuleMgr/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ModuleMgr/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    
+        public ActionResult Update(Entitys.Module u)
+        {
+            string result = "NO";
+            try
+            {
+                // TODO: Add update logic here
+                result = moduleSer.Save(u);
+
+            }
+            catch
+            {
+
+            }
+            return Content(result);
+        }
+        public ActionResult Add(Entitys.Module u)
+        {
+            string result = "NO";
+            try
+            {
+                // TODO: Add logic here
+                result = moduleSer.Add(u);
+
+            }
+            catch
+            {
+
+            }
+            return Content(result);
+        }
+
+       
+        public ActionResult Delete(string ids)
+        {
+            string result = "NO";
+            try
+            {
+                // TODO: Add Delete logic here
+                result = moduleSer.Delete(ids);
+            }
+            catch
+            {
+
+            }
+            return Content(result);
+        }
+        public string  GetJsonTree()
+        {
+            List<ModuleJson> list = LinqJsonTree(0);
+            list.Insert(0, new ModuleJson() { id =0, children = null,  parentId =0, text = "根节点" });
+            return JsonHelper.Instance.Serialize(list);
+            
+        }
+            /// <summary>
+            /// 递归
+            /// </summary>
+            /// <param name="list"></param>
+            /// <returns></returns>
+            //
+            private List<ModuleJson> LinqJsonTree(int parentId)
+        {
+            List<Entitys.Module> classlist = moduleSer.GetModules(1, 100).Where(m=>m.ParentId==parentId).ToList();
+
+            List<ModuleJson> jsonData = new List<ModuleJson>();
+            classlist.ForEach(item =>
+            {
+                jsonData.Add(new ModuleJson
+                {
+                    id = item.Id,
+                    children = LinqJsonTree(item.Id),
+                    parentId = item.ParentId,
+                    text = item.Name,
+                    url = string.Empty
+                });
+            });
+
+
+            return jsonData;
+        }
+    }
+}
